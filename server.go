b298e42a838f8bdf3f4c24a68c7b6d7070ba90f8
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-01 15:11:24 +0500

    Update server.go
    Add PlayerStore

--- server.go
+++ server.go
@@ -6,19 +6,16 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
-	fmt.Fprint(w, getPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
-func getPlayerScore(player string) string {
-	if player == "Floyd" {
-		return "10"
-	}
+type PlayerServer struct {
+	store PlayerStore
+}
 
-	if player == "Pepper" {
-		return "20"
-	}
-	return ""
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
