c6f5d1fce9d68e7e4a6679a7d67874c12ee04fd8
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-01 19:59:33 +0500

    Update main.go

--- main.go
+++ main.go
@@ -5,18 +5,8 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	log.Fatal(http.ListenAndServe(":9000", server))
 }
