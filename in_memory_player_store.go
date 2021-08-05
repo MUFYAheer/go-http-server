3e7ac0ab20463b2086376a2342cbd20a98f11f34
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-05 23:00:06 +0500

    Update in_memory_player_store.go
    Add GetLeague method on InMemoryPlayerStore

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -15,3 +15,10 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 func (i *InMemoryPlayerStore) RecordWin(name string) {
 	i.store[name]++
 }
+
+func (i *InMemoryPlayerStore) GetLeague() (league []Player) {
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return
+}
