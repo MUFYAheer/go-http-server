6159fe1e7dc685bfaaf421b1880ab4fd9aef814e
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-07 23:25:35 +0500

    Update file_system_store.go
    Refactor

--- file_system_store.go
+++ file_system_store.go
@@ -12,17 +12,10 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
-
-	info, err := file.Stat()
+	err := initializePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte(`[]`))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem initializing player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -35,6 +28,22 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initializePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte(`[]`))
+		file.Seek(0, 0)
+	}
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
