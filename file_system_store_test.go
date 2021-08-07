dd2b4ed5e89b20c9a70caf6ef4b0fea8cc9b4709
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-07 15:22:51 +0500

    Update file_system_store_test.go
    add "get player score" test

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -27,4 +27,20 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}
+		]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Cleo")
+		want := 10
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
 }
