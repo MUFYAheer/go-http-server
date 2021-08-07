e9a577ae664ce3ce8909eef942bfbb9de1d11674
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-07 18:10:53 +0500

    Update file_system_store_test.go
    Add test for RecordWin

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -45,6 +45,22 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertScoreEqual(t, got, want)
 	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}
+		]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Chris")
+
+		got := store.GetPlayerScore("Chris")
+		want := 34
+		assertScoreEqual(t, got, want)
+	})
 }
 
 func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
