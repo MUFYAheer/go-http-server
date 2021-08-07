57883f12fa8b5c108734b558f4fb458571c8987c
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-07 18:11:11 +0500

    Update file_system_store.go
    Add RecordWin

--- file_system_store.go
+++ file_system_store.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 )
 
@@ -12,6 +13,18 @@ func (f *FileSystemPlayerStore) GetLeague() (league []Player) {
 	f.database.Seek(0, 0)
 	league, _ = NewLeague(f.database)
 	return
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+			break
+		}
+	}
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(&league)
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) (wins int) {
