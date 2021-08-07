2e5513391748dd12518d3875be142fb2efe14612
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-07 21:21:57 +0500

    Update file_system_store.go
    Add NewFileSystemPlayerStore to read from disk just once

--- file_system_store.go
+++ file_system_store.go
@@ -7,28 +7,35 @@ import (
 
 type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
+	league   League
 }
 
-func (f *FileSystemPlayerStore) GetLeague() (league League) {
-	f.database.Seek(0, 0)
-	league, _ = NewLeague(f.database)
-	return
+func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
+	database.Seek(0, 0)
+	league, _ := NewLeague(database)
+	return &FileSystemPlayerStore{
+		database,
+		league,
+	}
+}
+
+func (f *FileSystemPlayerStore) GetLeague() League {
+	return f.league
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
-	league := f.GetLeague()
-	player := league.Find(name)
+	player := f.league.Find(name)
 	if player != nil {
 		player.Wins++
 	} else {
-		league = append(league, Player{name, 1})
+		f.league = append(f.league, Player{name, 1})
 	}
 	f.database.Seek(0, 0)
-	json.NewEncoder(f.database).Encode(&league)
+	json.NewEncoder(f.database).Encode(&f.league)
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	player := f.GetLeague().Find(name)
+	player := f.league.Find(name)
 	if player == nil {
 		return 0
 	}
