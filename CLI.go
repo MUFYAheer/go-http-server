52b858894ec01b800868ad97034416fadc388f6f
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-12 22:45:20 +0500

    Create CLI.go

---
+++ CLI.go
@@ -0,0 +1,14 @@
+package poker
+
+import (
+	"io"
+)
+
+type CLI struct {
+	playerStore PlayerStore
+	in          io.Reader
+}
+
+func (cli *CLI) PlayPoker() {
+	cli.playerStore.RecordWin("Chris")
+}
