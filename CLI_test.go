ee57a70b8ded4e13d064ea2ce205f4d6f9d6d33e
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-12 23:13:03 +0500

    Update CLI_test.go

--- CLI_test.go
+++ CLI_test.go
@@ -6,10 +6,21 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore, in}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := &CLI{playerStore, in}
 
-	cli.PlayPoker()
-	assertPlayerScore(t, playerStore, "Chris")
+		cli.PlayPoker()
+		assertPlayerScore(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := StubPlayerStore{}
+		cli := CLI{&playerStore, in}
+		cli.PlayPoker()
+
+		assertPlayerScore(t, &playerStore, "Cleo")
+	})
 }
