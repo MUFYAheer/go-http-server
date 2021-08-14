c644417f51004bdcb945f4fb6e2774f769ae941a
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 22:59:29 +0500

    Create BlindAlerter.go

---
+++ internal/poker/BlindAlerter.go
@@ -0,0 +1,23 @@
+package poker
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+type BlindAlerterFunc func(duration time.Duration, amount int)
+
+func (b BlindAlerterFunc) ScheduleAlertAt(duration time.Duration, amount int) {
+	b(duration, amount)
+}
+
+func StdOutAlerter(duration time.Duration, amount int) {
+	time.AfterFunc(duration, func() {
+		fmt.Fprintf(os.Stdout, "Blind is now %d\n", amount)
+	})
+}
