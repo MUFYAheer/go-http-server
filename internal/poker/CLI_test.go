5030f09a53b3efac23e306ad2a4f96e637dcb9a9
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 22:39:35 +0500

    Update CLI_test.go

--- internal/poker/CLI_test.go
+++ internal/poker/CLI_test.go
@@ -9,18 +9,21 @@ import (
 	"github.com/MUFYAheer/go-http-server/internal/poker"
 )
 
-type SpyBlindAlerter struct {
-	alerts []struct {
-		scheduledAt time.Duration
-		amount      int
-	}
+type scheduledAlert struct {
+	at     time.Duration
+	amount int
 }
 
-func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.alerts = append(s.alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
+func (s scheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.amount, s.at)
+}
+
+type SpyBlindAlerter struct {
+	alerts []scheduledAlert
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(at time.Duration, amount int) {
+	s.alerts = append(s.alerts, scheduledAlert{at, amount})
 }
 
 func TestCLI(t *testing.T) {
@@ -50,10 +53,7 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		cases := []struct {
-			expectedScheduleTime time.Duration
-			expectedAmount       int
-		}{
+		cases := []scheduledAlert{
 			{0 * time.Second, 100},
 			{10 * time.Minute, 200},
 			{20 * time.Minute, 300},
@@ -67,22 +67,26 @@ func TestCLI(t *testing.T) {
 			{100 * time.Minute, 8000},
 		}
 
-		for i, c := range cases {
-			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+		for i, want := range cases {
+			t.Run(fmt.Sprint(want), func(t *testing.T) {
 				if len(blindAlerter.alerts) <= i {
-					t.Fatalf("alert %d was not scheduled %v", i, c.expectedScheduleTime)
+					t.Fatalf("alert %d was not scheduled %v", i, want.at)
 				}
 
-				alert := blindAlerter.alerts[i]
-
-				if alert.amount != c.expectedAmount {
-					t.Errorf("got amount %d, want %d", alert.amount, c.expectedAmount)
-				}
-
-				if alert.scheduledAt != c.expectedScheduleTime {
-					t.Errorf("got scheduled time at %v, want %v", alert.scheduledAt, c.expectedScheduleTime)
-				}
+				got := blindAlerter.alerts[i]
+				assertScheduledAlert(t, got, want)
 			})
 		}
 	})
+}
+
+func assertScheduledAlert(t testing.TB, got, want scheduledAlert) {
+	t.Helper()
+	if got.amount != want.amount {
+		t.Errorf("got amount %d, want %d", got.amount, want.amount)
+	}
+
+	if got.at != want.at {
+		t.Errorf("got scheduled time at %v, want %v", got.at, want.at)
+	}
 }
