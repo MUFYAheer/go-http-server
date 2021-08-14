95533982b25182e88265232c7f82ef69f149f7e6
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 18:10:24 +0500

    Create func FileSystemPlayerStoreFromFile

--- file_system_store.go
+++ file_system_store.go
@@ -12,6 +12,24 @@ type FileSystemPlayerStore struct {
 	league   League
 }
 
+func FileSystemPlayerStoreFromFile(path string) (PlayerStore, func(), error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem opening %s, %v", path, err)
+	}
+	closeFunc := func() {
+		db.Close()
+	}
+
+	store, err := NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem creating file player store, %v", err)
+	}
+
+	return store, closeFunc, nil
+}
+
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	err := initializePlayerDBFile(file)
 
