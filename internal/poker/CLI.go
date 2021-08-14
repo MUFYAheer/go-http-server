d1d7e2a6610457052817be09bf73952770dec654
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 22:59:53 +0500

    Update CLI.go

--- internal/poker/CLI.go
+++ internal/poker/CLI.go
@@ -7,10 +7,6 @@ import (
 	"time"
 )
 
-type BlindAlerter interface {
-	ScheduleAlertAt(duration time.Duration, amount int)
-}
-
 type CLI struct {
 	playerStore PlayerStore
 	scanner     *bufio.Scanner
