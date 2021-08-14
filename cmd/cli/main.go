88a20090df871c8de5a8a5ac780fea11e8aa9b2f
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 18:10:56 +0500

    Consume FileSystemPlayerStoreFromFile func

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,20 +10,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0644)
-
-	if err != nil {
-		log.Fatalf("problem opening %s, %v", dbFileName, err)
-	}
-	playerStore, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem initializing file system player store, %v", err)
-	}
-
-	game := poker.NewCLI(playerStore, os.Stdin)
-	game.PlayPoker()
+	poker.NewCLI(store, os.Stdin).PlayPoker()
 }
