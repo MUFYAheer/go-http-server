a2fd3b17e8a90056a149ba1255993e57306d0b2a
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 21:20:27 +0500

    Update CLI.go

--- internal/poker/CLI.go
+++ internal/poker/CLI.go
@@ -4,19 +4,25 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
+
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
 
 type CLI struct {
 	playerStore PlayerStore
 	scanner     *bufio.Scanner
+	alerter     BlindAlerter
 }
 
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
-	return &CLI{playerStore: store, scanner: bufio.NewScanner(in)}
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
+	return &CLI{playerStore: store, scanner: bufio.NewScanner(in), alerter: alerter}
 }
 
 func (cli *CLI) PlayPoker() {
-
+	cli.alerter.ScheduleAlertAt(5*time.Second, 100)
 	cli.playerStore.RecordWin(extractWinner(cli.readLine()))
 }
 
