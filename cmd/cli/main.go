182e2e58f7b50190017d9b3e2924ee9a8b54889b
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-14 10:43:01 +0500

    Update main.go to run application

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"poker"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	fmt.Print("Let's play poker")
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0644)
+
+	if err != nil {
+		log.Fatalf("problem opening %s, %v", dbFileName, err)
+	}
+	playerStore, err := poker.NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		log.Fatalf("problem initializing file system player store, %v", err)
+	}
+
+	game := poker.NewCLI(playerStore, os.Stdin)
+	game.PlayPoker()
 }
