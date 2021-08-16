a0f857ac5a666f22231bd78e3504215c4866b8fa
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-08-16 09:17:50 +0500

    Update CLI.go

--- internal/poker/CLI.go
+++ internal/poker/CLI.go
@@ -2,22 +2,32 @@ package poker
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strings"
 	"time"
 )
 
+const PlayerPrompt = "Please enter the number of players: "
+
 type CLI struct {
 	playerStore PlayerStore
 	scanner     *bufio.Scanner
+	out         io.Writer
 	alerter     BlindAlerter
 }
 
-func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
-	return &CLI{playerStore: store, scanner: bufio.NewScanner(in), alerter: alerter}
+func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter) *CLI {
+	return &CLI{
+		playerStore: store,
+		scanner:     bufio.NewScanner(in),
+		out:         out,
+		alerter:     alerter,
+	}
 }
 
 func (cli *CLI) PlayPoker() {
+	fmt.Fprint(cli.out, PlayerPrompt)
 	cli.scheduleBlindAlerts()
 	cli.playerStore.RecordWin(extractWinner(cli.readLine()))
 }
